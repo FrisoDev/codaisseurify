328ac156f57bf83bfe73408b2309e28f951b3a1e
FrisoDev <frisodevstoker@gmail.com>
2017-10-13 18:12:52 +0200

    cosmetics

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@ artist2 = Artist.create!( { name: "Bing", country: "Russia", act_since: 1909, bi
 artist3 = Artist.create!( { name: "Bong", country: "France", act_since: 2001, bio: "Someshit", genre: "Rockedgyshit", born: 1963})
 
 song1 = Song.create!( { name: "Song1", length_min: 4, length_sec: 34, artist: artist1})
+song2 = Song.create!( { name: "Song2", length_min: 7, length_sec: 21, artist: artist1})
