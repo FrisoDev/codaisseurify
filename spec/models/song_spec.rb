df62ba7f65fb57991b5d1c190f19bdde6de6d110
FrisoDev <frisodevstoker@gmail.com>
2017-10-15 16:17:25 +0200

    add style form less shit-ish

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
-describe "association with artist" do
-  let(:artist) { create :artist }
-
-  it "belongs to an artist" do
-    artist = artist.songs.build(name: "Shared")
-
-    expect(song.artist).to eq(artist)
+describe "validations" do
+  it "is invalid without a name" do
+    song = Song.new(name: "")
+    song.valid?
+    expect(song.errors).to have_key(:name)
   end
 end
