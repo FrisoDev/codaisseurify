290027ed60b1bb4beff15ac13be0965580aa24b8
FrisoDev <frisodevstoker@gmail.com>
2017-10-15 18:01:55 +0200

    sds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
-around_action :rescue_from_fk_contraint, only: [:destroy]
 
-def rescue_from_fk_contraint
-  begin
-    yield
-  rescue ActiveRecord::InvalidForeignKey
-    # Flash and render, render API json error... whatever
-  end
-end
+# def rescue_from_fk_contraint
+#   begin
+#     yield
+#   rescue ActiveRecord::InvalidForeignKey
+#     # Flash and render, render API json error... whatever
+#   end
+# end
